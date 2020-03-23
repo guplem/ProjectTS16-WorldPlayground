f5ce66e9f6984575903be12a00c1d22658aa1935
gillemp <guplem@gmail.com>
2020-03-23 14:07:45 +0100

    Basic world generation

---
+++ WorldPlayground/Assets/Scripts/Editor/TerrainManagerInspector.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(TerrainManager))]
+[CanEditMultipleObjects]
+public class TerrainManagerInspector : Editor
+{
+    //SerializedProperty lookAtPoint;
+    private TerrainManager terrainManager;
+    
+    void OnEnable()
+    {
+        //lookAtPoint = serializedObject.FindProperty("lookAtPoint");
+        terrainManager = (TerrainManager) target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        
+        //serializedObject.Update();
+
+        if(GUILayout.Button("Destroy world"))
+            terrainManager.DestroyWorld();
+        
+        if(GUILayout.Button("Generate world"))
+            terrainManager.GenerateWorld();
+
+        if (GUILayout.Button("Regenerate world"))
+        {
+            terrainManager.DestroyWorld();
+            terrainManager.GenerateWorld();
+        }
+
+        //serializedObject.ApplyModifiedProperties();
+    }
+}
--- WorldPlayground/Assets/Scripts/TerrainManager.cs
+++ WorldPlayground/Assets/Scripts/TerrainManager.cs
@@ -6,14 +6,34 @@ public class TerrainManager : MonoBehaviour
 {
     [SerializeField] private int terrainSize = 100;
     [SerializeField] private GameObject cube;
-    
+    [SerializeField] private int minSurfaceHeight = 64;
+    [SerializeField] private int maxSurfaceHeight = 120;
+    [SerializeField] private int seed;
+    [SerializeField] private float frequency = 100;
+
     void Start()
     {
-        for (int x = -terrainSize/2; x <= terrainSize/2; x++)
+        DestroyWorld();
+        GenerateWorld();
+    }
+
+    public void DestroyWorld()
+    {
+        Debug.Log("Destroying world.");
+        this.transform.DestroyImmediateAllChildren();
+    }
+    
+    public void GenerateWorld()
+    {
+        Debug.Log("Generating world.");
+        
+        for (int x = -terrainSize / 2; x <= terrainSize / 2; x++)
         {
-            for (int z = -terrainSize/2; z <= terrainSize/2; z++)
+            for (int z = -terrainSize / 2; z <= terrainSize / 2; z++)
             {
-                Instantiate(cube, new Vector3(x, 0, z), Quaternion.identity);
+                int y = Mathf.RoundToInt(minSurfaceHeight + Mathf.PerlinNoise(x/frequency+seed, z/frequency+seed) * (maxSurfaceHeight-minSurfaceHeight));    
+                
+                Instantiate(cube, new Vector3(x, y, z), Quaternion.identity, this.transform);
             }
         }
     }
