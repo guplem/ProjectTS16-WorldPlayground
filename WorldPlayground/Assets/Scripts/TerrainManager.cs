01ab3c28f4d414f454c694d5a22b53d79ae43c07
gillemp <guplem@gmail.com>
2020-03-23 11:48:21 +0100

    Basic flat generation

---
+++ WorldPlayground/Assets/Scripts/TerrainManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainManager : MonoBehaviour
+{
+    [SerializeField] private int terrainSize = 100;
+    [SerializeField] private GameObject cube;
+    
+    void Start()
+    {
+        for (int x = -terrainSize/2; x <= terrainSize/2; x++)
+        {
+            for (int z = -terrainSize/2; z <= terrainSize/2; z++)
+            {
+                Instantiate(cube, new Vector3(x, 0, z), Quaternion.identity);
+            }
+        }
+    }
+}
