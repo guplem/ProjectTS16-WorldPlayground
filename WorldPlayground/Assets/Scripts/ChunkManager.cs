e79b25d1a8b9d0597dc4799554db65cb37d77e6a
gillemp <guplem@gmail.com>
2020-03-28 10:49:15 +0100

    Restructured code

--- WorldPlayground/Assets/Scripts/ChunkManager.cs
+++ WorldPlayground/Assets/Scripts/ChunkManager.cs
@@ -46,7 +46,7 @@ public class ChunkManager : MonoBehaviour
         
         
         //Build the mesh with the chunk data //TODO: remove. Shouldn't happen 
-        GenerateChunkMesh();
+        UpdateMesh();
     }
 
     private void GenerateChunkData()
@@ -58,8 +58,53 @@ public class ChunkManager : MonoBehaviour
                         chunkData[x, y, z] = cube;
     }
 
-    private void GenerateChunkMesh()
+    
+    
+    
+    
+    
+    List<Vector3> verticesInMesh = new List<Vector3>(); // All mesh vertices
+    List<int> indexOfVerticesToFormTriangles = new List<int>(); // Index of the vertices that will form each of the triangles (that form all the faces). They must be in order (and grouped by 3) to form every triangle, so the total size must be a multiple of 3)
+    List<Vector2> uvs = new List<Vector2>(); // Texture mapping values (texture coordinates for each vertex with the same index as the coordinate in this list)
+    
+    private void UpdateMesh()
     {
-        meshFilter.mesh = Cube.GetCubeMesh();
+        GenerateMeshData();
+        meshFilter.mesh = GetChunkMeshWithCurrentData();
+    }
+
+    private void GenerateMeshData()
+    {
+		int vertexIndex = 0;
+		for (int face = 0; face < 6; face++) // 6 faces in a cube
+		{
+			for (int vertexNumber = 0; vertexNumber < 6; vertexNumber++) // 6 vertex per face
+			{
+				// Save the vertex position
+				int vertexIndexOfTriangle = VoxelData.verticesOfFace[face, vertexNumber];
+				verticesInMesh.Add(VoxelData.vertexPosition[vertexIndexOfTriangle]);
+				
+				// Assign the vertex to the correspondent triangle
+				indexOfVerticesToFormTriangles.Add(vertexIndex);
+				vertexIndex++;
+				
+				// Assign the texture coordinates for that vertex
+				uvs.Add(VoxelData.textureCoordinates[vertexNumber]);
+			}
+		}
+    }
+    
+    private Mesh GetChunkMeshWithCurrentData()
+    {
+        // Create a new mesh with all the information built before
+        Mesh mesh = new Mesh
+        {
+            vertices = verticesInMesh.ToArray(),
+            uv = uvs.ToArray(),
+            triangles = indexOfVerticesToFormTriangles.ToArray(),
+        };
+
+        mesh.RecalculateNormals();
+        return mesh;
     }
 }
